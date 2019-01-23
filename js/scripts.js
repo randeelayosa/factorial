24dfd60596d66879b30f7d3846ed40548b6e1a34
Jack Collins and Randee Layosa <randee@>
2019-01-23 15:19:37 -0800

    Add for loop to refactor code and compute factorial.

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,21 @@ $(document).ready(function() {
   var multiples = [];
   var factorial = 1;
 
+
   $("#factor-form").submit(function(event) {
   event.preventDefault();
   var userInput = parseInt($("#factorial").val());
-  multiples.push(userInput);
-  console.log(multiples)
 
-  multiples.forEach(function(multiple) {
-    factorial *= multiple;
+  for (var i = userInput; i > 0; i--) {
+    factorial *= i;
+  }
+
+  $("#result").append(factorial);
+  $("#result").show();
+  $("#factor-form").fadeToggle();
+
+  console.log(userInput)
+  console.log(factorial)
+
   });
 });
-});
-
-
-
-
-
-
-  //
-  // $("#factor-form").submit(function(event) {
-  // event.preventDefault();
-  // var factorial = 1;
-  // var multiples = [5,4,3,2,1];
-  //
-  // multiples.forEach(function(multiple) {
-  //   factorial *= multiple;
-  // });
